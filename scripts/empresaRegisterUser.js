34fd85ae8352355fd9b407d2c88aa6a9ab96d9a3
Ximit <im.ximit@gmail.com>
2019-09-23 23:02:34 -0300

    07

--- scripts/empresaRegisterUser.js
+++ scripts/empresaRegisterUser.js
@@ -44,7 +44,7 @@ Fabric_Client.newDefaultKeyValueStore({
     fabric_ca_client = new Fabric_CA_Client('http://localhost:7054', null, '', crypto_suite);
 
     // first check to see if the admin is already enrolled
-    return fabric_client.getUserContext('admin', true);
+    return fabric_client.getUserContext(user, true);
 }).then((user_from_store) => {
     if (user_from_store && user_from_store.isEnrolled()) {
         console.log('Successfully loaded admin from persistence');
@@ -55,17 +55,17 @@ Fabric_Client.newDefaultKeyValueStore({
 
     // at this point we should have the admin user
     // first need to register the user with the CA server
-    return fabric_ca_client.register({ enrollmentID: user, affiliation: null, role: 'client' }, admin_user);
+    return fabric_ca_client.register({ enrollmentID: user + 'cli', affiliation: null, role: 'client' }, admin_user);
 }).then((secret) => {
     // next we need to enroll the user with CA server
-    console.log('Successfully registered ' + user + ' - secret:' + secret);
+    console.log('Successfully registered ' + user + 'cli' + ' - secret:' + secret);
 
-    return fabric_ca_client.enroll({ enrollmentID: user, enrollmentSecret: secret });
+    return fabric_ca_client.enroll({ enrollmentID: user + 'cli', enrollmentSecret: secret });
 }).then((enrollment) => {
     console.log('Successfully enrolled member ' + user + ' "' + user + '" ');
     return fabric_client.createUser(
         {
-            username: user,
+            username: user + 'cli1',
             mspid: 'EmpresaMSP',
             cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
         });
